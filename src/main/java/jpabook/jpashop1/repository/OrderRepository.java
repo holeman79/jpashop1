db153076a7721202c72bc7f6553e35d5815e026f
holeman79 <holeman79@gmail.com>
2020-06-14 23:55:28 +0900

    쿼리 dsl 추가

--- src/main/java/jpabook/jpashop1/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop1/repository/OrderRepository.java
@@ -1,6 +1,11 @@
 package jpabook.jpashop1.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop1.domain.Order;
+import jpabook.jpashop1.domain.OrderStatus;
+import jpabook.jpashop1.domain.QMember;
+import jpabook.jpashop1.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -24,14 +29,14 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch) {
-
-
-        String jpql = "select o from Order o join o.member m";
-        return em.createQuery(jpql, Order.class)
-                .setMaxResults(1000)    // 최대 1000건
-                .getResultList();
-    }
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//
+//        String jpql = "select o from Order o join o.member m";
+//        return em.createQuery(jpql, Order.class)
+//                .setMaxResults(1000)    // 최대 1000건
+//                .getResultList();
+//    }
 //
 //    /**
 //     * JPA Criteria
@@ -41,6 +46,31 @@ public class OrderRepository {
 //        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
 //
 //    }
+
+    public List<Order> findAll(OrderSearch orderSearch){
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query.select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus orderStatus){
+        if(orderStatus == null)
+            return null;
+        return QOrder.order.status.eq(orderStatus);
+    }
+
+    private BooleanExpression nameLike(String name){
+        if(!StringUtils.hasText(name)) return null;
+        return QMember.member.name.like(name);
+    }
 
     public List<Order> findAllByString(OrderSearch orderSearch) {
         //language=JPAQL
